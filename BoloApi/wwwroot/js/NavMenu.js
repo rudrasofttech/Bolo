48b11f0cbe3d7e13cedd42f7ba6bd380ef472f41
Raj Kiran Singh <rajkiran.singh@rudrasofttech.com>
2020-07-01 10:48:00 +0530

    Fix navbar right alignment

--- BoloApi/wwwroot/js/NavMenu.js
+++ BoloApi/wwwroot/js/NavMenu.js
@@ -205,7 +205,7 @@ class NavMenu extends React.Component {
                                 <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse " id="navbarMainMenu">
-                                <ul className="navbar-nav flex-grow">
+                                <ul className="nav navbar-nav ml-auto">
                                     <li className="nav-item">
                                         <a className="nav-link text-light" href="/Chat">Chat</a>
                                     </li>
