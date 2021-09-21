2476864d794da07d6bba535355dd37bdd95b0d13
Raj Kiran Singh <rajkiran.singh@rudrasofttech.com>
2021-09-21 08:00:11 +0530

    Changes the title from meetins to discusions. App and Layout js still experimental.

---
+++ BoloApi/wwwroot/js/App.js
@@ -0,0 +1,59 @@
+﻿
+//Auth Context
+const AuthContext = React.createContext({
+    token: null,
+    setToken: (data) => { },
+    data: null,
+    setData: (data) => { },
+})
+
+//Use Auth Context
+function useAuthContext() {
+    return useContext(AuthContext);
+}
+
+class Layout extends React.Component {
+    static displayName = Layout.name;
+
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: localStorage.getItem("token"),
+            data: localStorage.getItem("user")
+        }
+    }
+
+    setToken = (data) => {
+        this.setState({ token: data });
+    }
+
+    setData = (data) => {
+        this.setState({ data: data });
+    }
+
+    render() {
+        return (
+            <AuthContext.Provider value={{
+                data: this.state.data,
+                token: this.state.token,
+                setToken: this.setToken,
+                setData: this.setData
+            }}>
+                <Layout>
+                    <React.Route exact path='/' component={Welcome} />
+                </Layout>
+            </AuthContext.Provider>
+        );
+    }
+}
---
+++ BoloApi/wwwroot/js/Layout.js
@@ -0,0 +1,14 @@
+﻿class Layout extends React.Component {
+    static displayName = Layout.name;
+
+    render() {
+        return (
+            <div>
+                <NavMenu />
+                <Container>
+                    {this.props.children}
+                </Container>
+            </div>
+        );
+    }
+}
